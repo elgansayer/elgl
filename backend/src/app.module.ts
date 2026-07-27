b277e6c568e3e0e8746558ca4a32af853574fffc
Your Name <you@example.com>
2026-07-27 10:29:16 +0000

    ci: automated pipeline cycle complete

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -43,7 +43,7 @@ import { validationSchema } from './config/validation.schema';
       validationSchema,
     }),
     ScheduleModule.forRoot(),
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+
     ThrottlerModule.forRoot([
       {
         ttl: 60000,
