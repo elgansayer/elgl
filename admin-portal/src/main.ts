c53356c66337cf00b5bae932ef72719eb8afd311
Elgan Sayer <elgan.sayer@gmail.com>
2026-08-15 20:53:14 +0100

    feat(admin): provide HTTP client for admin context

--- admin-portal/src/main.ts
+++ admin-portal/src/main.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
@@ -5,5 +6,5 @@ import { AdminAppComponent } from './app/admin-app.component';
 import { ADMIN_ROUTES } from './app/admin.routes';
 
 bootstrapApplication(AdminAppComponent, {
-  providers: [provideRouter(ADMIN_ROUTES)],
+  providers: [provideHttpClient(), provideRouter(ADMIN_ROUTES)],
 }).catch((error: unknown) => console.error(error));
