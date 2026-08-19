2e6f2f8c5e6bca4ed6b42efa25d2e165d3c8a605
Elgan Sayer <elgan.sayer@gmail.com>
2026-08-19 12:49:04 +0100

    refactor(storage): keep R2 gateway module explicitly scoped

--- backend/src/cloudflare-r2/r2.module.ts
+++ backend/src/cloudflare-r2/r2.module.ts
@@ -1,7 +1,6 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { R2Service } from './r2.service';
 
-@Global()
 @Module({
   providers: [R2Service],
   exports: [R2Service],
