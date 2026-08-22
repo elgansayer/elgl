96e53cef3709252f40a30d72f54eedfde883a7d0
Elgan Sayer <elgan.sayer@gmail.com>
2026-08-23 00:36:05 +0100

    refactor(challenges): remove non-atomic monetisation dependency

--- backend/src/language-challenges/language-challenges.module.ts
+++ backend/src/language-challenges/language-challenges.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { LanguageChallengesController } from './language-challenges.controller';
 import { LanguageChallengesService } from './language-challenges.service';
-import { MonetisationModule } from '../monetisation/monetisation.module';
 
 @Module({
-  imports: [MonetisationModule],
   controllers: [LanguageChallengesController],
   providers: [LanguageChallengesService],
   exports: [LanguageChallengesService],
