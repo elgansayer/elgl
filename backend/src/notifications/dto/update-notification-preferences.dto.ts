e6903ac12fed731154a11e906e2bb0a622e4fa6e
Elgan Sayer <elgan.sayer@gmail.com>
2026-08-26 00:28:46 +0100

    fix(notifications): validate channel preference matrix

--- backend/src/notifications/dto/update-notification-preferences.dto.ts
+++ backend/src/notifications/dto/update-notification-preferences.dto.ts
@@ -1,6 +1,40 @@
-import { IsOptional, IsBoolean, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class LegacyPreferenceChannelDto {
+  @IsBoolean()
+  readonly push!: boolean;
+
+  @IsBoolean()
+  readonly badge!: boolean;
+}
 
 export class UpdateNotificationPreferencesDto {
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => LegacyPreferenceChannelDto)
+  readonly direct_messages?: LegacyPreferenceChannelDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => LegacyPreferenceChannelDto)
+  readonly groups?: LegacyPreferenceChannelDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => LegacyPreferenceChannelDto)
+  readonly likes?: LegacyPreferenceChannelDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => LegacyPreferenceChannelDto)
+  readonly voice_rooms?: LegacyPreferenceChannelDto;
+
   @IsOptional()
   @IsBoolean()
   readonly do_not_disturb?: boolean;
