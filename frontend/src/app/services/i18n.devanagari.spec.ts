d8c2865918c30ff1120c18a888c7c5b1ce7ce0d8
Elgan Sayer <elgan.sayer@gmail.com>
2026-08-25 16:42:45 +0100

    fix(test): import Angular document token

--- frontend/src/app/services/i18n.devanagari.spec.ts
+++ frontend/src/app/services/i18n.devanagari.spec.ts
@@ -1,3 +1,4 @@
+import { DOCUMENT } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
