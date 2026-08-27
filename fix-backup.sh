24886a8938925ab95a495eb52093d624e683d95e
Elgan Sayer <elgan.sayer@gmail.com>
2026-08-28 00:54:23 +0100

    chore(frontend): remove accidental repair helper

--- fix-backup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i "s|import { Component, OnInit, signal, inject } from '@angular/core';|import { Component, OnInit, signal, inject } from '@angular/core';\nimport { environment } from '../../../environments/environment';|g" frontend/src/app/pages/settings/backup-restore.component.ts
